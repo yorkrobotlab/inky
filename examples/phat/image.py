7f7174807436114868bb20ad9a69313dc64b62de
Russell Joyce <russell.joyce@york.ac.uk>
2020-05-29 17:03:49 +0100

    Replaced 'ArUco' example script with generic image display example

--- examples/phat/image.py
+++ examples/phat/image.py
@@ -3,14 +3,10 @@
 import os
 import argparse
 from PIL import Image
-from inky import InkyWHAT, InkyPHAT
+from inky import InkyPHAT
 
 
-print("""Inky pHAT: ArUco tag test
-
-aruco_mip_36h12_00000
-
-""")
+print("""Inky pHAT: Image test""")
 
 # Get the current path
 
@@ -19,21 +15,21 @@ PATH = os.path.dirname(__file__)
 # Command line arguments to set display type and colour
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--colour', '-c', type=str, default="black", choices=["red", "black", "yellow"], help="ePaper display colour")
-parser.add_argument('--tag', '-t', type=str, default="aruco_mip_36h12_00000", help="Tag name")
+parser.add_argument('--colour', '-c', type=str, default="red", choices=["red", "black", "yellow"], help="ePaper display colour")
+parser.add_argument('--lut', '-l', type=str, default=None, help="override ePaper drawing LUT")
+parser.add_argument('--image', '-i', type=str, default="resources/aruco_mip_36h12_00000_212x104.png", help="Image path")
 args = parser.parse_args()
 colour = args.colour
-tag = args.tag
+lut = args.lut
+image = args.image
 
-# Set up the correct display and scaling factors
+# Set up the display
+inky_display = InkyPHAT(colour, lut)
 
-inky_display = InkyPHAT("black")
-# inky_display.set_border(inky_display.BLACK)
-
-# Set the image to show, and reduce colour palette to black and white
-
+# Set the image to show, and reduce colour palette to black/white or black/white/red
 palette = Image.new('P', (1, 1))
 if colour == 'black':
+    colours = 2
     palette.putpalette(
     [
         255, 255, 255,   # 0 = White
@@ -41,6 +37,7 @@ if colour == 'black':
     ] + [0, 0, 0] * 254  # Zero fill the rest of the 256 colour palette
     )
 else:
+    colours = 3
     palette.putpalette(
     [
         255, 255, 255,   # 0 = White
@@ -49,9 +46,9 @@ else:
     ] + [0, 0, 0] * 253  # Zero fill the rest of the 256 colour palette
     )
 
-img = Image.open(os.path.join(PATH, "resources", tag + "_212x104.png"))
-img = img.quantize(colors=2, palette=palette)
+img = Image.open(image)
+img = img.quantize(colors=colours, palette=palette)
 
-# Display the tag image
+# Display the image
 inky_display.set_image(img)
 inky_display.show()
